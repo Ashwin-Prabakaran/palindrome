9bafafeec704ec3a94d4f649a6a7b3e8200adcb5
Ashwin-Prabakaran <ashwin6694@gmail.com>
2021-05-19 00:21:09 +0530

    Palindrome

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a word to check if it is Palindrome:");
+			string inputWord = Console.ReadLine();
+			try
+			{
+				char[] inputWordArray = inputWord.ToCharArray();
+				Array.Reverse(inputWordArray);
+				string reverse = new string(inputWordArray);
+
+				if (inputWord.Equals(reverse, StringComparison.OrdinalIgnoreCase))
+					Console.WriteLine(inputWord + "is a Palindrome");
+				else
+					Console.WriteLine(inputWord + "is not a Palindrome");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+			Console.ReadLine();
+
+		}
+	}
+}
